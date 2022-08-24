26bb0385b1c7e8157acb3d985bccb356ccb4ae26
Raphael Scheible <raphael.scheible@tum.de>
2022-08-24 16:36:05 +0200

    added user_id

--- tests/mockup.data.ts
+++ tests/mockup.data.ts
@@ -2,7 +2,8 @@ export const SINGLE_TODO = {
   "id": 2,
   "todo": "item_2",
   "private": true,
-  "mine": false
+  "mine": false,
+  "user_id": 3
 };
 
 export const TODO_LIST = [
@@ -10,36 +11,42 @@ export const TODO_LIST = [
     "id": 1,
     "todo": "item_1",
     "private": false,
-    "mine": true
+    "mine": true,
+    "user_id": 1
   },
   {
     "id": 2,
     "todo": "item_2",
     "private": true,
-    "mine": false
+    "mine": false,
+    "user_id": 3
   },
   {
     "id": 3,
     "todo": "item_3",
     "private": false,
-    "mine": false
+    "mine": false,
+    "user_id": 2
   },
   {
     "id": 4,
     "todo": "item_4",
     "private": true,
-    "mine": true
+    "mine": true,
+    "user_id": 1
   },
   {
     "id": 5,
     "todo": "item_5",
     "private": true,
-    "mine": false
+    "mine": false,
+    "user_id": 3
   },
   {
     "id": 6,
     "todo": "item_6",
     "private": false,
-    "mine": false
+    "mine": false,
+    "user_id": 2
   }
 ];
