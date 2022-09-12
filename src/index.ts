9d8f31faf92851aa57e831836e482a48e478f0d4
Raphael Scheible <raphael.scheible@tum.de>
2022-09-12 18:38:25 +0200

    included prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { fetchUtils, DataProvider } from 'ra-core';
-import { PrimaryKey, PostgRestOperator, getPrimaryKey, parseFilters, 
-         getOrderBy, dataWithId, getQuery, getKeyData, encodeId } from "./urlBuilder";
+import {
+    PrimaryKey,
+    PostgRestOperator,
+    getPrimaryKey,
+    parseFilters,
+    getOrderBy,
+    dataWithId,
+    getQuery,
+    getKeyData,
+    encodeId,
+} from './urlBuilder';
 /**
  * Maps react-admin queries to a postgrest REST API
  *
@@ -37,244 +46,249 @@ import { PrimaryKey, PostgRestOperator, getPrimaryKey, parseFilters,
  * export default App;
  */
 
-
 const defaultPrimaryKeys = new Map<string, PrimaryKey>();
 
-export default (apiUrl : string, httpClient = fetchUtils.fetchJson, defaultListOp : PostgRestOperator = 'eq', 
-                primaryKeys: Map<string, PrimaryKey> = defaultPrimaryKeys): DataProvider => ({
-  getList: (resource, params) => {
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
+export default (
+    apiUrl: string,
+    httpClient = fetchUtils.fetchJson,
+    defaultListOp: PostgRestOperator = 'eq',
+    primaryKeys: Map<string, PrimaryKey> = defaultPrimaryKeys
+): DataProvider => ({
+    getList: (resource, params) => {
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
 
-    const { page, perPage } = params.pagination;
-    const { field, order } = params.sort;
-    const parsedFilter = parseFilters(params.filter, defaultListOp);
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const parsedFilter = parseFilters(params.filter, defaultListOp);
 
-    const query = {
-      order: getOrderBy(field, order, primaryKey),
-      offset: String((page - 1) * perPage),
-      limit: String(perPage),
-      // append filters
-      ...parsedFilter
-    };
+        const query = {
+            order: getOrderBy(field, order, primaryKey),
+            offset: String((page - 1) * perPage),
+            limit: String(perPage),
+            // append filters
+            ...parsedFilter,
+        };
 
-    // add header that Content-Range is in returned header
-    const options = {
-      headers: new Headers({
-        Accept: 'application/json',
-        Prefer: 'count=exact'
-      })
-    };
+        // add header that Content-Range is in returned header
+        const options = {
+            headers: new Headers({
+                Accept: 'application/json',
+                Prefer: 'count=exact',
+            }),
+        };
 
-    const url = `${apiUrl}/${resource}?${new URLSearchParams(query)}`;
+        const url = `${apiUrl}/${resource}?${new URLSearchParams(query)}`;
 
-    return httpClient(url, options).then(({ headers, json }) => {
-      if (!headers.has('content-range')) {
-        throw new Error(
-          `The Content-Range header is missing in the HTTP Response. The postgREST data provider expects 
+        return httpClient(url, options).then(({ headers, json }) => {
+            if (!headers.has('content-range')) {
+                throw new Error(
+                    `The Content-Range header is missing in the HTTP Response. The postgREST data provider expects 
           responses for lists of resources to contain this header with the total number of results to build 
           the pagination. If you are using CORS, did you declare Content-Range in the Access-Control-Expose-Headers header?`
-        );
-      }
-      return {
-        data: json.map(obj => dataWithId(obj, primaryKey)),
-        total: parseInt(
-          headers
-            .get('content-range')
-            .split('/')
-            .pop(),
-          10
-        )
-      };
-    });
-  },
+                );
+            }
+            return {
+                data: json.map(obj => dataWithId(obj, primaryKey)),
+                total: parseInt(
+                    headers.get('content-range').split('/').pop(),
+                    10
+                ),
+            };
+        });
+    },
 
-  getOne: (resource, params) => {
-    const id = params.id;
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
-    
-    const query = getQuery(primaryKey, id, resource);
-    
-    const url = `${apiUrl}/${resource}?${query}`;
+    getOne: (resource, params) => {
+        const id = params.id;
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
 
-    return httpClient(url, {
-      headers: new Headers({ 'accept': 'application/vnd.pgrst.object+json' }),
-    }).then(({ json }) => ({
-      data: dataWithId(json, primaryKey),
-    }))
-  },
+        const query = getQuery(primaryKey, id, resource);
 
-  getMany: (resource, params) => {
-    const ids = params.ids;
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
+        const url = `${apiUrl}/${resource}?${query}`;
 
-    const query = getQuery(primaryKey, ids, resource);
-      
-    const url = `${apiUrl}/${resource}?${query}`;
+        return httpClient(url, {
+            headers: new Headers({
+                accept: 'application/vnd.pgrst.object+json',
+            }),
+        }).then(({ json }) => ({
+            data: dataWithId(json, primaryKey),
+        }));
+    },
 
-    return httpClient(url).then(({ json }) => ({ data: json.map(data => dataWithId(data, primaryKey)) }));
-  },
+    getMany: (resource, params) => {
+        const ids = params.ids;
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
 
-  getManyReference: (resource, params) => {
-    const { page, perPage } = params.pagination;
-    const { field, order } = params.sort;
-    const parsedFilter = parseFilters(params.filter, defaultListOp);
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
+        const query = getQuery(primaryKey, ids, resource);
 
-    const query = params.target ? {
-      [params.target]: `eq.${params.id}`,
-      order: getOrderBy(field, order, primaryKey),
-      offset: String((page - 1) * perPage),
-      limit: String(perPage),
-      ...parsedFilter,
-    }:{
-      order: getOrderBy(field, order, primaryKey),
-      offset: String((page - 1) * perPage),
-      limit: String(perPage),
-      ...parsedFilter,
-    };
+        const url = `${apiUrl}/${resource}?${query}`;
 
-    // add header that Content-Range is in returned header
-    const options = {
-      headers: new Headers({
-        Accept: 'application/json',
-        Prefer: 'count=exact'
-      })
-    }
+        return httpClient(url).then(({ json }) => ({
+            data: json.map(data => dataWithId(data, primaryKey)),
+        }));
+    },
 
-    const url = `${apiUrl}/${resource}?${new URLSearchParams(query)}`;
+    getManyReference: (resource, params) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const parsedFilter = parseFilters(params.filter, defaultListOp);
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
 
-    return httpClient(url, options).then(({ headers, json }) => {
-      if (!headers.has('content-range')) {
-        throw new Error(
-          `The Content-Range header is missing in the HTTP Response. The postgREST data provider expects 
+        const query = params.target
+            ? {
+                  [params.target]: `eq.${params.id}`,
+                  order: getOrderBy(field, order, primaryKey),
+                  offset: String((page - 1) * perPage),
+                  limit: String(perPage),
+                  ...parsedFilter,
+              }
+            : {
+                  order: getOrderBy(field, order, primaryKey),
+                  offset: String((page - 1) * perPage),
+                  limit: String(perPage),
+                  ...parsedFilter,
+              };
+
+        // add header that Content-Range is in returned header
+        const options = {
+            headers: new Headers({
+                Accept: 'application/json',
+                Prefer: 'count=exact',
+            }),
+        };
+
+        const url = `${apiUrl}/${resource}?${new URLSearchParams(query)}`;
+
+        return httpClient(url, options).then(({ headers, json }) => {
+            if (!headers.has('content-range')) {
+                throw new Error(
+                    `The Content-Range header is missing in the HTTP Response. The postgREST data provider expects 
           responses for lists of resources to contain this header with the total number of results to build 
           the pagination. If you are using CORS, did you declare Content-Range in the Access-Control-Expose-Headers header?`
-        );
-      }
-      return {
-        data: json.map(data => dataWithId(data, primaryKey)),
-        total: parseInt(
-          headers
-            .get('content-range')
-            .split('/')
-            .pop(),
-          10
-        ),
-      };
-    });
-  },
+                );
+            }
+            return {
+                data: json.map(data => dataWithId(data, primaryKey)),
+                total: parseInt(
+                    headers.get('content-range').split('/').pop(),
+                    10
+                ),
+            };
+        });
+    },
 
-  update: (resource, params) => {
-    const { id, data } = params;
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
+    update: (resource, params) => {
+        const { id, data } = params;
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
 
-    const query = getQuery(primaryKey, id, resource);
+        const query = getQuery(primaryKey, id, resource);
 
-    const primaryKeyData = getKeyData(primaryKey, data);
-
-    const url = `${apiUrl}/${resource}?${query}`;
-
-    const body = JSON.stringify({
-      ...data,
-      ...primaryKeyData
-    });
-
-    return httpClient(url, {
-      method: 'PATCH',
-      headers: new Headers({
-        'Accept': 'application/vnd.pgrst.object+json',
-        'Prefer': 'return=representation',
-        'Content-Type': 'application/json'
-      }),
-      body,
-    }).then(({ json }) => ({ data: dataWithId(json, primaryKey) }));
-  },
-
-  updateMany: (resource, params) => {
-    const ids = params.ids;
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
-
-    const query = getQuery(primaryKey, ids, resource);
-
-    const body = JSON.stringify(
-      params.data.map(obj => {
-        const { id, ...data } = obj;
         const primaryKeyData = getKeyData(primaryKey, data);
 
-        return {
-          ...data,
-          ...primaryKeyData
-        };
-      })
-    );
+        const url = `${apiUrl}/${resource}?${query}`;
 
-    const url = `${apiUrl}/${resource}?${query}`;
+        const body = JSON.stringify({
+            ...data,
+            ...primaryKeyData,
+        });
 
-    return httpClient(url, {
-      method: 'PATCH',
-      headers: new Headers({
-        'Prefer': 'return=representation',
-        'Content-Type': 'application/json',
-      }),
-      body,
-    }).then(({ json }) => ({
-      data: json.map(data => encodeId(data, primaryKey))
-    }));
-  },
+        return httpClient(url, {
+            method: 'PATCH',
+            headers: new Headers({
+                Accept: 'application/vnd.pgrst.object+json',
+                Prefer: 'return=representation',
+                'Content-Type': 'application/json',
+            }),
+            body,
+        }).then(({ json }) => ({ data: dataWithId(json, primaryKey) }));
+    },
 
-  create: (resource, params) => {
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
+    updateMany: (resource, params) => {
+        const ids = params.ids;
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
 
-    const url = `${apiUrl}/${resource}`;
+        const query = getQuery(primaryKey, ids, resource);
 
-    return httpClient(url, {
-      method: 'POST',
-      headers: new Headers({
-        'Accept': 'application/vnd.pgrst.object+json',
-        'Prefer': 'return=representation',
-        'Content-Type': 'application/json'
-      }),
-      body: JSON.stringify(params.data),
-    }).then(({ json }) => ({
-      data: {
-        ...params.data,
-        id: encodeId(json, primaryKey)
-      }
-    }));
-  },
+        const body = JSON.stringify(
+            params.data.map(obj => {
+                const { id, ...data } = obj;
+                const primaryKeyData = getKeyData(primaryKey, data);
 
-  delete: (resource, params) => {
-    const id = params.id;
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
-    
-    const query = getQuery(primaryKey, id, resource);
+                return {
+                    ...data,
+                    ...primaryKeyData,
+                };
+            })
+        );
 
-    const url = `${apiUrl}/${resource}?${query}`;
+        const url = `${apiUrl}/${resource}?${query}`;
 
-    return httpClient(url, {
-      method: 'DELETE',
-      headers: new Headers({
-        'Accept': 'application/vnd.pgrst.object+json',
-        'Prefer': 'return=representation',
-        'Content-Type': 'application/json'
-      }),
-    }).then(({ json }) => ({ data: dataWithId(json, primaryKey) }));
-  },
+        return httpClient(url, {
+            method: 'PATCH',
+            headers: new Headers({
+                Prefer: 'return=representation',
+                'Content-Type': 'application/json',
+            }),
+            body,
+        }).then(({ json }) => ({
+            data: json.map(data => encodeId(data, primaryKey)),
+        }));
+    },
 
-  deleteMany: (resource, params) => {
-    const ids = params.ids;
-    const primaryKey = getPrimaryKey(resource, primaryKeys);
+    create: (resource, params) => {
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
 
-    const query = getQuery(primaryKey, ids, resource);
-      
-    const url = `${apiUrl}/${resource}?${query}`;
+        const url = `${apiUrl}/${resource}`;
 
-    return httpClient(url, {
-      method: 'DELETE',
-      headers: new Headers({
-        'Prefer': 'return=representation',
-        'Content-Type': 'application/json'
-      }),
-    }).then(({ json }) => ({ data: json.map(data => encodeId(data, primaryKey)) }));
-  },
+        return httpClient(url, {
+            method: 'POST',
+            headers: new Headers({
+                Accept: 'application/vnd.pgrst.object+json',
+                Prefer: 'return=representation',
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify(params.data),
+        }).then(({ json }) => ({
+            data: {
+                ...params.data,
+                id: encodeId(json, primaryKey),
+            },
+        }));
+    },
+
+    delete: (resource, params) => {
+        const id = params.id;
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
+
+        const query = getQuery(primaryKey, id, resource);
+
+        const url = `${apiUrl}/${resource}?${query}`;
+
+        return httpClient(url, {
+            method: 'DELETE',
+            headers: new Headers({
+                Accept: 'application/vnd.pgrst.object+json',
+                Prefer: 'return=representation',
+                'Content-Type': 'application/json',
+            }),
+        }).then(({ json }) => ({ data: dataWithId(json, primaryKey) }));
+    },
+
+    deleteMany: (resource, params) => {
+        const ids = params.ids;
+        const primaryKey = getPrimaryKey(resource, primaryKeys);
+
+        const query = getQuery(primaryKey, ids, resource);
+
+        const url = `${apiUrl}/${resource}?${query}`;
+
+        return httpClient(url, {
+            method: 'DELETE',
+            headers: new Headers({
+                Prefer: 'return=representation',
+                'Content-Type': 'application/json',
+            }),
+        }).then(({ json }) => ({
+            data: json.map(data => encodeId(data, primaryKey)),
+        }));
+    },
 });
--- src/urlBuilder.ts
+++ src/urlBuilder.ts
@@ -1,183 +1,218 @@
-import {Identifier} from 'ra-core';
+import { Identifier } from 'ra-core';
 
 /**
  * Interface to clearly seperate the operator like < oder >
  * from the name and the value of the filter
  */
 interface PostgRestFilter {
-  name: string;
-  operator?: PostgRestOperator;
-  value: string;
+    name: string;
+    operator?: PostgRestOperator;
+    value: string;
 }
 
 /**
- * List of all possible operators in PostgRest 
+ * List of all possible operators in PostgRest
  * (https://postgrest.org/en/stable/api.html#operators)
  */
 const postgrestOperators = [
-  "eq",
-  "gt",
-  "gte",
-  "lt",
-  "lte",
-  "neq",
-  "like",
-  "ilike",
-  "in",
-  "is",
-  "fts",
-  "plfts",
-  "phfts",
-  "wfts",
-  "cs",
-  "cd",
-  "ov",
-  "sl",
-  "sr",
-  "nxr",
-  "nxl",
-  "adj",
-  "not",
-  "or",
-  "and"
+    'eq',
+    'gt',
+    'gte',
+    'lt',
+    'lte',
+    'neq',
+    'like',
+    'ilike',
+    'in',
+    'is',
+    'fts',
+    'plfts',
+    'phfts',
+    'wfts',
+    'cs',
+    'cd',
+    'ov',
+    'sl',
+    'sr',
+    'nxr',
+    'nxl',
+    'adj',
+    'not',
+    'or',
+    'and',
 ] as const;
-
 
 export type PostgRestOperator = typeof postgrestOperators[number];
 
-export const parseFilters = (filter : Array<string>, defaultListOp : PostgRestOperator) => {
-  let result = {};
-  Object.keys(filter).forEach(function (key) {
-    // key: the name of the object key
+export const parseFilters = (
+    filter: Array<string>,
+    defaultListOp: PostgRestOperator
+) => {
+    let result = {};
+    Object.keys(filter).forEach(function (key) {
+        // key: the name of the object key
 
-    const splitKey = key.split('@');
-    const operation : PostgRestOperator = 
-      splitKey.length == 2 ? splitKey[1] as PostgRestOperator : defaultListOp;
+        const splitKey = key.split('@');
+        const operation: PostgRestOperator =
+            splitKey.length == 2
+                ? (splitKey[1] as PostgRestOperator)
+                : defaultListOp;
 
-    let values : Array<string>;
-    if (operation in ['like', 'ilike']) {
-      // we split the search term in words
-      values = filter[key].trim().split(' ');
-    } else {
-      values = [filter[key]];
-    }
-
-    values.forEach(value => {
-      // if operator is intentionally blank, rpc syntax 
-      let op = operation.includes('like') ? `${operation}.*${value}*` : operation.length == 0 ? `${value}` : `${operation}.${value}`;
-
-      if (result[splitKey[0]] === undefined) {
-        // first operator for the key, we add it to the dict
-        result[splitKey[0]] = op;
-      }
-      else {
-        if (!Array.isArray(result[splitKey[0]])) {
-          // second operator, we transform to an array
-          result[splitKey[0]] = [result[splitKey[0]], op]
+        let values: Array<string>;
+        if (operation in ['like', 'ilike']) {
+            // we split the search term in words
+            values = filter[key].trim().split(' ');
         } else {
-          // third and subsequent, we add to array
-          result[splitKey[0]].push(op);
+            values = [filter[key]];
         }
-      }
+
+        values.forEach(value => {
+            // if operator is intentionally blank, rpc syntax
+            let op = operation.includes('like')
+                ? `${operation}.*${value}*`
+                : operation.length == 0
+                ? `${value}`
+                : `${operation}.${value}`;
+
+            if (result[splitKey[0]] === undefined) {
+                // first operator for the key, we add it to the dict
+                result[splitKey[0]] = op;
+            } else {
+                if (!Array.isArray(result[splitKey[0]])) {
+                    // second operator, we transform to an array
+                    result[splitKey[0]] = [result[splitKey[0]], op];
+                } else {
+                    // third and subsequent, we add to array
+                    result[splitKey[0]].push(op);
+                }
+            }
+        });
     });
 
-  });
-
-  return result;
-}
+    return result;
+};
 
 // compound keys capability
 export type PrimaryKey = Array<string>;
 
-export const getPrimaryKey = (resource: string, primaryKeys: Map<string, PrimaryKey>) => {
-  return primaryKeys.get(resource) || ['id'];
-}
-
-export const decodeId = (id: Identifier, primaryKey: PrimaryKey): string[] => {
-  if (isCompoundKey(primaryKey)) {
-    return JSON.parse(id.toString());
-  } else {
-    return [id.toString()];
-  }
-}
-
-export const encodeId = (data: any, primaryKey: PrimaryKey): Identifier => {
-  if (isCompoundKey(primaryKey)) {
-    return JSON.stringify(primaryKey.map(key => data[key]));
-  } else {
-    return data[primaryKey[0]];
-  }
-}
-
-export const dataWithId = (data: any, primaryKey: PrimaryKey) => {
-  if (primaryKey === ['id']) {
-    return data;
-  }
-
-  return Object.assign(data, {
-    id: encodeId(data, primaryKey)
-  });
-}
-
-export const isCompoundKey = (primaryKey: PrimaryKey): Boolean => {
-  return primaryKey.length > 1;
-}
-
-export const getQuery = (primaryKey: PrimaryKey, ids: Identifier | Array<Identifier>, resource: string): string => {
-  if (Array.isArray(ids) && ids.length > 1) {
-    // no standardized query with multiple ids possible for rpc endpoints which are api-exposed database functions
-    if (resource.startsWith('rpc/')) {
-      console.error('PostgREST\'s rpc endpoints are not intended to be handled as views. Therefore, no query generation for multiple key values implemented!');
-
-      return;
-    }
-
-    if (isCompoundKey(primaryKey)) {
-      return `or=(
-            ${ids.map(id => {
-              const primaryKeyParams = decodeId(id, primaryKey);
-              return `and(${primaryKey.map((key, i) => `${key}.eq.${primaryKeyParams[i]}`).join(',')})`;
-            })}
-          )`;
-    } else {
-      return new URLSearchParams({ [primaryKey[0]]: `in.(${ids.join(',')})` }).toString();
-    }
-  } else {
-    // if ids is one Identifier
-    const id: Identifier = ids.toString();
-    const primaryKeyParams = decodeId(id, primaryKey);
-
-    if (isCompoundKey(primaryKey)) {
-      if (resource.startsWith('rpc/'))
-        return `${primaryKey.map((key: string, i: any) => `${key}=${primaryKeyParams[i]}`).join('&')}`;
-      else
-        return `and=(${primaryKey.map((key: string, i: any) => `${key}.eq.${primaryKeyParams[i]}`).join(',')})`;
-    } else {
-      return new URLSearchParams([[primaryKey[0], `eq.${id}`]]).toString();
-    }
-  }
-}
-
-export const getKeyData = (primaryKey: PrimaryKey, data: object): object => {
-  if (isCompoundKey(primaryKey)) {
-    return primaryKey.reduce(
-      (keyData, key) => ({
-        ...keyData,
-        [key]: data[key]
-      }),
-      {});
-  } else {
-    return { [primaryKey[0]]: data[primaryKey[0]] };
-  }
-}
-
-export const getOrderBy = (field: string, order: string, primaryKey: PrimaryKey) => {
-  if (field == 'id') {
-    return primaryKey.map(key => (`${key}.${order.toLowerCase()}`)).join(',');
-  } else {
-    return `${field}.${order.toLowerCase()}`;
-  }
+export const getPrimaryKey = (
+    resource: string,
+    primaryKeys: Map<string, PrimaryKey>
+) => {
+    return primaryKeys.get(resource) || ['id'];
 };
 
-// TODO: create function such as generatePostgRestUrl(), taking filter props, 
+export const decodeId = (id: Identifier, primaryKey: PrimaryKey): string[] => {
+    if (isCompoundKey(primaryKey)) {
+        return JSON.parse(id.toString());
+    } else {
+        return [id.toString()];
+    }
+};
+
+export const encodeId = (data: any, primaryKey: PrimaryKey): Identifier => {
+    if (isCompoundKey(primaryKey)) {
+        return JSON.stringify(primaryKey.map(key => data[key]));
+    } else {
+        return data[primaryKey[0]];
+    }
+};
+
+export const dataWithId = (data: any, primaryKey: PrimaryKey) => {
+    if (primaryKey === ['id']) {
+        return data;
+    }
+
+    return Object.assign(data, {
+        id: encodeId(data, primaryKey),
+    });
+};
+
+export const isCompoundKey = (primaryKey: PrimaryKey): Boolean => {
+    return primaryKey.length > 1;
+};
+
+export const getQuery = (
+    primaryKey: PrimaryKey,
+    ids: Identifier | Array<Identifier>,
+    resource: string
+): string => {
+    if (Array.isArray(ids) && ids.length > 1) {
+        // no standardized query with multiple ids possible for rpc endpoints which are api-exposed database functions
+        if (resource.startsWith('rpc/')) {
+            console.error(
+                "PostgREST's rpc endpoints are not intended to be handled as views. Therefore, no query generation for multiple key values implemented!"
+            );
+
+            return;
+        }
+
+        if (isCompoundKey(primaryKey)) {
+            return `or=(
+            ${ids.map(id => {
+                const primaryKeyParams = decodeId(id, primaryKey);
+                return `and(${primaryKey
+                    .map((key, i) => `${key}.eq.${primaryKeyParams[i]}`)
+                    .join(',')})`;
+            })}
+          )`;
+        } else {
+            return new URLSearchParams({
+                [primaryKey[0]]: `in.(${ids.join(',')})`,
+            }).toString();
+        }
+    } else {
+        // if ids is one Identifier
+        const id: Identifier = ids.toString();
+        const primaryKeyParams = decodeId(id, primaryKey);
+
+        if (isCompoundKey(primaryKey)) {
+            if (resource.startsWith('rpc/'))
+                return `${primaryKey
+                    .map(
+                        (key: string, i: any) => `${key}=${primaryKeyParams[i]}`
+                    )
+                    .join('&')}`;
+            else
+                return `and=(${primaryKey
+                    .map(
+                        (key: string, i: any) =>
+                            `${key}.eq.${primaryKeyParams[i]}`
+                    )
+                    .join(',')})`;
+        } else {
+            return new URLSearchParams([
+                [primaryKey[0], `eq.${id}`],
+            ]).toString();
+        }
+    }
+};
+
+export const getKeyData = (primaryKey: PrimaryKey, data: object): object => {
+    if (isCompoundKey(primaryKey)) {
+        return primaryKey.reduce(
+            (keyData, key) => ({
+                ...keyData,
+                [key]: data[key],
+            }),
+            {}
+        );
+    } else {
+        return { [primaryKey[0]]: data[primaryKey[0]] };
+    }
+};
+
+export const getOrderBy = (
+    field: string,
+    order: string,
+    primaryKey: PrimaryKey
+) => {
+    if (field == 'id') {
+        return primaryKey.map(key => `${key}.${order.toLowerCase()}`).join(',');
+    } else {
+        return `${field}.${order.toLowerCase()}`;
+    }
+};
+
+// TODO: create function such as generatePostgRestUrl(), taking filter props,
 //       ids etc. and return the generated PostgREST uri
