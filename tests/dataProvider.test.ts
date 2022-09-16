9a43d8642e83b4956f6482526a3d27ed24045396
Christiaan Westerbeek <chris@devotis.nl>
2022-09-16 15:37:18 +0200

    Apply new prettier config to formatting
    Please turn on Format On Save in VScode

--- tests/dataProvider.test.ts
+++ tests/dataProvider.test.ts
@@ -1,36 +1,35 @@
-import { fetchUtils, SORT_ASC, SORT_DESC } from "ra-core";
+import { fetchUtils, SORT_ASC, SORT_DESC } from 'ra-core';
 
-import { SINGLE_TODO, TODO_LIST } from "./mockup.data";
-import dataProvider from "../src/index";
+import { SINGLE_TODO, TODO_LIST } from './mockup.data';
+import dataProvider from '../src/index';
 
 type HTTPClientMock = typeof fetchUtils.fetchJson;
 
-describe("test dataProvider", () => {
-    const BASE_URL = "http://localhost:3000";
+describe('test dataProvider', () => {
+    const BASE_URL = 'http://localhost:3000';
 
-    test("minimal arguments for dataprovider", () => {
+    test('minimal arguments for dataprovider', () => {
         const t = () => {
             dataProvider(BASE_URL)
-                .getOne("Patient", { id: SINGLE_TODO.id })
-                .then((response) => response != undefined);
+                .getOne('Patient', { id: SINGLE_TODO.id })
+                .then(response => response != undefined);
         };
         expect(t).not.toThrow(TypeError);
     });
 
-    describe("getList", () => {
-        test("test page offset, sort and return value", () => {
+    describe('getList', () => {
+        test('test page offset, sort and return value', () => {
             const httpClient = createHTTPClientMock(
                 200,
                 undefined,
                 undefined,
                 TODO_LIST
             );
-
         });
     });
 
-    describe("getOne", () => {
-        test("with Entity ID", () => {
+    describe('getOne', () => {
+        test('with Entity ID', () => {
             const httpClient = createHTTPClientMock(
                 200,
                 undefined,
@@ -38,16 +37,13 @@ describe("test dataProvider", () => {
                 SINGLE_TODO
             );
         });
-
-
     });
-
 });
 
 /**
  * Helper method for creating a jest function which mocks a HTTP client.
  */
- function createHTTPClientMock(
+function createHTTPClientMock(
     expectedStatus: number,
     expectedHeaders?: Headers,
     expectedBody?: string,
