08dda1cce016f30c9a57b7e86e54bf0bea898190
Christiaan Westerbeek <chris@devotis.nl>
2022-10-04 19:50:37 +0200

    Add test suite for isCompoundKey #42

--- tests/urlBuilder.test.ts
+++ tests/urlBuilder.test.ts
@@ -1,12 +1,22 @@
-import { getOrderBy, PrimaryKey } from '../src/urlBuilder';
+import { getOrderBy, isCompoundKey, PrimaryKey } from '../src/urlBuilder';
 
-const primaryKey: PrimaryKey = ['id'];
+const primaryKeySingle: PrimaryKey = ['id'];
+const primaryKeyMulti: PrimaryKey = ['id', 'type'];
+
+describe('isCompoundKey', () => {
+    it('should return false if the primaryKey consists of a single column', () => {
+        expect(isCompoundKey(primaryKeySingle)).toBe(false);
+    });
+    it('should return true if the primaryKey consists of multiple columns', () => {
+        expect(isCompoundKey(primaryKeyMulti)).toBe(true);
+    });
+});
 
 describe('getOrderBy', () => {
     it('should return an order by string for an id column', () => {
-        expect(getOrderBy('id', 'DESC', primaryKey)).toBe('id.desc');
+        expect(getOrderBy('id', 'DESC', primaryKeySingle)).toBe('id.desc');
     });
     it('should return an order by string for any other column', () => {
-        expect(getOrderBy('xxx', 'ASC', primaryKey)).toBe('xxx.asc');
+        expect(getOrderBy('xxx', 'ASC', primaryKeySingle)).toBe('xxx.asc');
     });
 });
