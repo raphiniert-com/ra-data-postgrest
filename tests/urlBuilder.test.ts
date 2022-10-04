e56cd2d2d7b8c701400f852542a210a014e9b052
Christiaan Westerbeek <chris@devotis.nl>
2022-10-04 20:02:03 +0200

    Add test suite for getPrimaryKey #42

--- tests/urlBuilder.test.ts
+++ tests/urlBuilder.test.ts
@@ -1,7 +1,32 @@
-import { getOrderBy, isCompoundKey, PrimaryKey } from '../src/urlBuilder';
+import {
+    getOrderBy,
+    getPrimaryKey,
+    isCompoundKey,
+    PrimaryKey,
+} from '../src/urlBuilder';
 
 const primaryKeySingle: PrimaryKey = ['id'];
 const primaryKeyMulti: PrimaryKey = ['id', 'type'];
+
+describe('getPrimaryKey', () => {
+    const resourcePimaryKeys = new Map<string, PrimaryKey>();
+    resourcePimaryKeys.set('contacts', primaryKeyMulti);
+    resourcePimaryKeys.set('licenses', ['license_id']);
+
+    it('should return the special primary for any resource in the resourcePimaryKeys map', () => {
+        expect(getPrimaryKey('contacts', resourcePimaryKeys)).toEqual(
+            primaryKeyMulti
+        );
+        expect(getPrimaryKey('licenses', resourcePimaryKeys)).toEqual([
+            'license_id',
+        ]);
+    });
+    it("should return the regular primary ['id'] for any other resource", () => {
+        expect(getPrimaryKey('todos', resourcePimaryKeys)).toEqual(
+            primaryKeySingle
+        );
+    });
+});
 
 describe('isCompoundKey', () => {
     it('should return false if the primaryKey consists of a single column', () => {
