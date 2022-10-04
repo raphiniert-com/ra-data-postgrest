e85c66ed970178520bd1a097e445bae49336581f
Christiaan Westerbeek <chris@devotis.nl>
2022-10-04 19:44:39 +0200

    Fix test names #42

--- tests/urlBuilder.test.ts
+++ tests/urlBuilder.test.ts
@@ -3,10 +3,10 @@ import { getOrderBy, PrimaryKey } from '../src/urlBuilder';
 const primaryKey: PrimaryKey = ['id'];
 
 describe('getOrderBy', () => {
-    it('ordering by the id column returns string', () => {
+    it('should return an order by string for an id column', () => {
         expect(getOrderBy('id', 'DESC', primaryKey)).toBe('id.desc');
     });
-    it('ordering by another column returns string', () => {
+    it('should return an order by string for any other column', () => {
         expect(getOrderBy('xxx', 'ASC', primaryKey)).toBe('xxx.asc');
     });
 });
