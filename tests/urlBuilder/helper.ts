5fa0d1b3c8059fc014f3a188caa36529719ee439
Christiaan Westerbeek <chris@devotis.nl>
2022-10-27 19:23:19 +0200

    Fix code comments

--- tests/urlBuilder/helper.ts
+++ tests/urlBuilder/helper.ts
@@ -2,7 +2,7 @@ export const qs = (queryParams: Record<string, any>) =>
     new URLSearchParams(queryParams).toString();
 
 /**
- * To encode uri components to be RFC 3986 -compliant - which encodes the characters !'()* - you can use:
+ * To encode uri components to be RFC 3986-compliant - which encodes the characters !'()*
  * @url https://stackoverflow.com/questions/44429173/javascript-encodeuri-failed-to-encode-round-bracket
  */
 const fixedEncodeURIComponent = (str: string) =>
